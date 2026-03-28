3f74e0f50ae924687db3f9f475eb1d7d2813ac14
kajeevan1992 <kajeevan1992@icloud.com>
2026-03-28 21:25:05 +0000

    Add files via upload

---
+++ db.ts
@@ -0,0 +1,5 @@
+import { NextResponse } from "next/server";
+import { getJobs } from "../../../lib/db";
+export async function GET(){
+  return NextResponse.json({jobs:getJobs()});
+}
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,13 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
+export const dynamic="force-dynamic";
+import { getJobs } from "../../lib/db";
+import { UploadForm } from "../../components/upload-form";
+export default function Page(){
+  const jobs=getJobs();
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <UploadForm/>
+      <pre>{JSON.stringify(jobs,null,2)}</pre>
+    </div>
+  );
+}
